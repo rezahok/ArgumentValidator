0498ea64f1cbd6ebd3d9492084b279404bead030
Rezaul Hoque <rezahok@gmail.com>
2016-08-12 20:00:22 +0200

    Argument Validators and Tests

---
+++ ArgumentValidator/ArgumentValidator/Throw.cs
@@ -0,0 +1,40 @@
+﻿namespace ArgumentValidator
+{
+    using System;
+
+    /// <summary>
+    /// Provides methods to do argument validations.
+    /// </summary>
+    public static class Throw
+    {
+        /// <summary>
+        /// Throws <exception cref="ArgumentException"/> if argument is null or empty.
+        /// </summary>
+        /// <param name="argumentValue">The argument value.</param>
+        /// <param name="argumentName">The argument name.</param>
+        /// <exception cref="ArgumentException">An argument exception</exception>
+        public static void IfNullOrEmpty(string argumentValue, string argumentName)
+        {
+            IfNull(argumentValue, argumentName);
+
+            if (argumentValue.Trim().Length == 0)
+            {
+                throw new ArgumentException("Should not be empty", argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Throws <exception cref="ArgumentNullException"/> if argument is null.
+        /// </summary>
+        /// <typeparam name="T">Type of the argument</typeparam>
+        /// <param name="argumentValue">The argument value.</param>
+        /// <param name="argumentName">The argument name.</param>
+        public static void IfNull<T>(T argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName, "Should not be null");
+            }
+        }
+    }
+}
---
+++ ArgumentValidator/ArgumentValidatorTests/ThrowTests.cs
@@ -0,0 +1,81 @@
+﻿namespace ArgumentValidator.Tests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Testing the argument validations.
+    /// </summary>
+    [TestClass]
+    public class ThrowTests
+    {
+        /// <summary>
+        /// IfNullOrEmpty throws <exception cref="ArgumentException"/> when the argument is empty.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IfNullOrEmpty_Empty_ThrowsArgumentException()
+        {
+            // Arrange
+            var argument = string.Empty;
+
+            // Act
+            Throw.IfNullOrEmpty(argument, nameof(argument));
+        }
+
+        /// <summary>
+        /// IfNullOrEmpty throws <exception cref="ArgumentNullException"/> when the argument is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IfNullOrEmpty_Null_ThrowsArgumentNullException()
+        {
+            // Arrange
+            string argument = null;
+
+            // Act
+            Throw.IfNullOrEmpty(argument, nameof(argument));
+        }
+
+        /// <summary>
+        /// IfNullOrEmpty does not throw any exception when argument is not null and not empty.
+        /// </summary>
+        [TestMethod]
+        public void IfNullOrEmpty_NotNullNotEmpty_NoExceptionThrown()
+        {
+            // Arrange
+            string argument = "dummy";
+
+            // Act
+            Throw.IfNullOrEmpty(argument, nameof(argument));
+        }
+
+        /// <summary>
+        /// IfNull throws <exception cref="ArgumentNullException"/> when the argument is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IfNull_Null_ThrowsArgumentNullException()
+        {
+            // Arrange
+            object argument = null;
+
+            // Act
+            Throw.IfNull(argument, nameof(argument));
+        }
+
+        /// <summary>
+        /// IfNull does not throw any exception when argument is not null.
+        /// </summary>
+        [TestMethod]
+        public void IfNull_NotNull_NoExceptionThrown()
+        {
+            // Arrange
+            object argument = 42;
+
+            // Act
+            Throw.IfNull(argument, nameof(argument));
+        }
+    }
+}
